58b12ed42ca25f16cde50d5ef761995f11dbd771
Veronica <almeida.vrc@gmail.com>
2022-09-29 14:04:11 -0300

    Cria componente do Card

--- src/app/components/thoughts/list-thought/list-thought.component.ts
+++ src/app/components/thoughts/list-thought/list-thought.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-thought',
   templateUrl: './list-thought.component.html',
-  styleUrls: ['./list-thought.component.css']
+  styleUrls: ['./list-thought.component.css'],
 })
 export class ListThoughtComponent implements OnInit {
+  listThoughts = [
+    {
+      content: 'I Love Coca-Cola',
+      authorship: 'Vê',
+      model: 'modelo1',
+    },
+    {
+      content: 'Passo intormações para o componente filho',
+      authorship: 'Componente pai',
+      model: 'modelo2',
+    },
+    {
+      content: 'Minha propriedade é decorada com @Input',
+      authorship: 'Componente filho',
+      model: 'modelo3',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-thought',
   templateUrl: './thought.component.html',
-  styleUrls: ['./thought.component.css']
+  styleUrls: ['./thought.component.css'],
 })
 export class ThoughtComponent implements OnInit {
+  @Input() thought = {
+    content: 'I Love Angular',
+    authorship: 'Vê',
+    model: 'modelo3',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
