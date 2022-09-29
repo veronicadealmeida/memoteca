b802f6436a5a68e7a527393ab4192ead0868a4c6
Veronica <almeida.vrc@gmail.com>
2022-09-29 13:34:53 -0300

    Altera nome da pasta para thoughts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CreateThoughtComponent } from './components/thought/create-thought/create-thought.component';
-import { ListThoughtComponent } from './components/thought/list-thought/list-thought.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
 
 const routes: Routes = [
   {
